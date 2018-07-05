99c459af2a74e344e143a4c669dc7c6b2034077e
574549756 <574549756@qq.com>
2018-07-05 17:24:34 +0800

    rebuild

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/lalu-markdown/dist',
+    assetsPublicPath: '/lalu-markdown/dist/',
 
     /**
      * Source Maps
